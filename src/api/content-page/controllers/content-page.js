36d57f809864866b215b831866efe05e7fbf77a1
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-04-20 16:59:28 +0800

    Update content page

--- src/api/content-page/controllers/content-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  content-page controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::content-page.content-page');
--- src/api/content-page/services/content-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * content-page service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::content-page.content-page');

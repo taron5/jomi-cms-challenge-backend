010c81bd5ebbf0c3651e8ffc09fbf5b6bed7231d
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-04-20 17:20:22 +0800

    Test

--- config/database.js
+++ config/database.js
@@ -1,10 +1,15 @@
-const path = require('path');
+const path = require("path");
+console.log(process.env.DATABASE_FILENAME);
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "sqlite",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
     },
     useNullAsDefault: true,
   },
